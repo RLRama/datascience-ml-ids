da5c8c120957a11c103f4cb4c685d44e3312bdcd
RLRama <rl.ramiro11@gmail.com>
2022-10-26 00:39:12 -0300

    Arreglando imports

--- 0_📈_Proyecto_de_Machine_Learning.py
+++ 0_📈_Proyecto_de_Machine_Learning.py
@@ -1,25 +1,8 @@
-from turtle import width
 import streamlit as st
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-import chart_studio.plotly as py
-import plotly.tools as tls
-import plotly.graph_objs as go
-import cufflinks as cf
-from pylab import rcParams
-from bokeh.plotting import figure, output_file, show, output_notebook
-from bokeh.models import ColumnDataSource
-from bokeh.palettes import Spectral6
-from bokeh.models import HoverTool
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score, precision_score, recall_score
-from sklearn.metrics import confusion_matrix
-from sklearn.metrics import classification_report
 
 st.set_page_config(
     page_title="Predicción con ML",
