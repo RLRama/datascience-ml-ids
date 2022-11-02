bb134d0d0cb6102c5c98373be9a78c5274ba6623
RLRama <rl.ramiro11@gmail.com>
2022-11-02 02:15:17 -0300

    BasicPage2Set1

--- pages/2_📉_Visualización de gráficos.py
+++ pages/2_📉_Visualización de gráficos.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+from sklearn.preprocessing import LabelEncoder
 
 st.set_page_config(
     page_title="Vista de gráficos",
