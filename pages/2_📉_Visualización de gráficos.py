bf2609785bd24e2097c8d04b46623f022de1e6f8
RLRama <rl.ramiro11@gmail.com>
2022-11-02 01:42:13 -0300

    BasePage2

---
+++ pages/2_📉_Visualización de gráficos.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+st.set_page_config(
+    page_title="Vista de gráficos",
+    page_icon="🤖",
+)
+
+st.image(
+    "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/apple/325/abacus_1f9ee.png",
+    width=100
+)
+
