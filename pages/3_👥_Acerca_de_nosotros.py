f45fb2edf259c09cdbcb8b1c7d80239bbec5789a
RLRama <rl.ramiro11@gmail.com>
2022-11-02 05:54:02 -0300

    Profiles

--- pages/3_👥_Acerca_de_nosotros.py
+++ pages/3_👥_Acerca_de_nosotros.py
@@ -10,3 +10,21 @@ st.image(
     width=100
 )
 st.title('Equipo de trabajo')
+
+tab1, tab2, tab3, tab4 = st.tabs(["Cano Ángel Rodrigo", "Dominguez Sotomayor Santiago Ismael", "Molina Leguiza Juan Cruz", "Rios Lopez Ramiro Ignacio"])
+
+with tab1:
+   st.header("A cat")
+   st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
+
+with tab2:
+   st.header("A dog")
+   st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
+
+with tab3:
+   st.header("An owl")
+   st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
+
+with tab4:
+   st.header("An owl")
+   st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
