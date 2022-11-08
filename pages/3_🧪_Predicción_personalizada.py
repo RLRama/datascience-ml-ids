7c301b628f1e7c43d52276030631730f08ec44a0
RLRama <rl.ramiro11@gmail.com>
2022-11-08 06:28:07 -0300

    SampleTest1

--- pages/3_🧪_Predicción_personalizada.py
+++ pages/3_🧪_Predicción_personalizada.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import sklearn
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
