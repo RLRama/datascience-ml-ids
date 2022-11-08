7fbb76f1db41842eb3b0fad0caf4468f839b54e2
RLRama <rl.ramiro11@gmail.com>
2022-11-08 05:47:39 -0300

    SampleTest1

--- pages/3_🧪_Predicción_personalizada.py
+++ pages/3_🧪_Predicción_personalizada.py
@@ -106,7 +106,9 @@ sp_concave_points_worst = st.slider('concave_points_worst (Puntos cóncavos de t
 sp_symmetry_worst = st.slider('symmetry_worst (Simetría de tejido)', min_value=0.1565, max_value=0.6638, value=0.2900755711775044)
 sp_fractal_dimension_worst = st.slider('fractal_dimension_worst (Dmiensión fractal de tejido)', min_value=0.05504, max_value=0.2075, value=0.0839458172231986)
 
-prediction = forest.predict([sp_radius_mean,sp_texture_mean,sp_perimeter_mean,sp_area_mean,sp_smoothness_mean,sp_compactness_mean,sp_concavity_mean,sp_concave_points_mean,sp_symmetry_mean,sp_fractal_dimension_mean,sp_radius_se,sp_texture_se,sp_perimeter_se,sp_area_se,sp_smoothness_se,sp_compactness_se,sp_concavity_se,sp_concave_points_se,sp_symmetry_se,sp_fractal_dimension_se,sp_radius_worst,sp_texture_worst,sp_perimeter_worst,sp_area_worst,sp_smoothness_worst,sp_compactness_worst,sp_concavity_worst,sp_concave_points_worst,sp_symmetry_worst,sp_fractal_dimension_worst])[0]
+new_sample = np.array([sp_radius_mean,sp_texture_mean,sp_perimeter_mean,sp_area_mean,sp_smoothness_mean,sp_compactness_mean,sp_concavity_mean,sp_concave_points_mean,sp_symmetry_mean,sp_fractal_dimension_mean,sp_radius_se,sp_texture_se,sp_perimeter_se,sp_area_se,sp_smoothness_se,sp_compactness_se,sp_concavity_se,sp_concave_points_se,sp_symmetry_se,sp_fractal_dimension_se,sp_radius_worst,sp_texture_worst,sp_perimeter_worst,sp_area_worst,sp_smoothness_worst,sp_compactness_worst,sp_concavity_worst,sp_concave_points_worst,sp_symmetry_worst,sp_fractal_dimension_worst]).reshape(1, -1)
+
+prediction = forest.predict(new_sample)[0]
 
 if st.button('Generar predicción'):
     if prediction=="1":
