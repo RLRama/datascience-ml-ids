4e9b6837ed2645cef286c7f97a7a936be15aaa3f
Dominguez Sotomayor Santiago Ismael <santiuni04@gmail.com>
2022-11-07 21:46:25 -0300

    Datos Rodri

--- pages/4_👥_Acerca_de_nosotros.py
+++ pages/4_👥_Acerca_de_nosotros.py
@@ -14,8 +14,28 @@ st.title('Equipo de trabajo')
 tab1, tab2, tab3, tab4 = st.tabs(["Cano Ángel Rodrigo", "Dominguez Sotomayor Santiago Ismael", "Molina Leguiza Juan Cruz", "Rios Lopez Ramiro Ignacio"])
 
 with tab1:
-   st.header("A cat")
+   
    st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
+      st.markdown(
+      """
+      ### Nombre
+      - Cano Angel Rodrigo
+      ---
+      ### Matrícula
+      - EISI821
+      ---
+      ### Fecha de Nacimiento
+      - 07/11/2000
+      ---
+      ### Redes Sociales
+      - [Perfil en Instagram](https://www.instagram.com/cano.rodrigo10/)
+      - [Perfil en Github](https://www.linkedin.com/in/rodrigo-cano-4bb070219/)
+      - [Perfil en Linkedin](https://github.com/Canoro10)
+      ---
+      ### Correo Electrónico
+      -  rodrigocano765@gmail.com
+      """
+   )
 
 with tab2:
    
